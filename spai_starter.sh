d37da395a8eac3d8a573d6613fef1198e922d969
Evan Cope <copev313@gmail.com>
2025-11-30 01:52:13 +0000

    Successful Run #6: SPAI Test Scores on All Sampled Datasets

---
+++ spai_starter.sh
@@ -0,0 +1,4 @@
+cd ~/Reveal-AI/spai && python -m spai infer \
+ --input ~/Reveal-AI/src/data/sample_scaled/test/ai/ \
+ --output ~/Reveal-AI/src/logs \
+ --model ~/Reveal-AI/spai/spai/weights/spai.pth
